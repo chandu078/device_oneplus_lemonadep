1731459b078b80067ff68c2fa3e120356ab72e00
Omkar Chandorkar <gotenksIN@aosip.dev>
2022-05-19 19:54:34 +0800

    lemonadep: Add PixelSetupWizardResTarget
    [cjh1249131356]: Extracted translations from PixelSetupWizard.apk
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- device.mk
+++ device.mk
@@ -24,7 +24,8 @@ PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget \
-    OPlusWifiResTarget
+    OPlusWifiResTarget \
+    PixelSetupWizardResTarget
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
