a8af6495cc7b29a620b162cd55a55f43251fe5b7
chandu078 <chandudyavanapelli03@gmail.com>
2022-09-27 15:57:30 +0000

    lemonadep: Set 420 screen density * 450 too big...
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ include device/oneplus/sm8350-common/BoardConfigCommon.mk
 DEVICE_PATH := device/oneplus/lemonadep
 
 # Display
-TARGET_SCREEN_DENSITY := 450
+TARGET_SCREEN_DENSITY := 420
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
